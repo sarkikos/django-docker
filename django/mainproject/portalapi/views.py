6ffd416a35b73b581e3788e7249bd1a72df43f11
lauri.hellsten <lauri.hellsten@csc.fi>
2019-03-29 10:00:40 +0200

    Version increased

--- django/mainproject/portalapi/views.py
+++ django/mainproject/portalapi/views.py
@@ -3,6 +3,6 @@ from django.http import JsonResponse
 # Create your views here.
 def get_version(request):
   versiondata = {
-    'version': '0.2'
+    'version': '0.3'
   }
   return JsonResponse(versiondata)
